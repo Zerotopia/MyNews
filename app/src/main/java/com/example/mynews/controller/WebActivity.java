34c3dc04f992315dce7bf30e3c59551d97e30abd
Zerotopia <guillaume.cano42@hotmail.fr>
2019-12-28 15:56:30 +0100

    finish webActivity

--- app/src/main/java/com/example/mynews/controller/WebActivity.java
+++ app/src/main/java/com/example/mynews/controller/WebActivity.java
@@ -2,12 +2,28 @@ package com.example.mynews.controller;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.webkit.WebView;
+
+import com.example.mynews.R;
+
+import static com.example.mynews.model.Article.NYT_HOME_URL;
 
 public class WebActivity extends AppCompatActivity {
+
+    private WebView mWebView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_web);
+
+        Intent intent = getIntent();
+        Bundle extra = intent.getExtras();
+        String url = (extra != null) ? extra.getString("url") : NYT_HOME_URL;
+
+        mWebView = findViewById(R.id.webactivity_webview);
+        mWebView.loadUrl(url);
     }
 }
