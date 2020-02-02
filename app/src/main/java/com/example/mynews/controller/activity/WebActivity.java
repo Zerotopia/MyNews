58b1efab709bd77e97bc36db5aae5a8d4de70027
Zerotopia <guillaume.cano42@hotmail.fr>
2020-02-02 20:42:07 +0100

    Add gestion of empty url in WebActivity

--- app/src/main/java/com/example/mynews/controller/activity/WebActivity.java
+++ app/src/main/java/com/example/mynews/controller/activity/WebActivity.java
@@ -7,10 +7,12 @@ import android.os.Bundle;
 import android.webkit.WebView;
 
 import com.example.mynews.R;
+import com.example.mynews.controller.fragment.AlertDialogFragment;
 
 import static com.example.mynews.model.Article.NYT_HOME_URL;
+import static com.example.mynews.model.Article.UNDEFINED;
 
-public class WebActivity extends AppCompatActivity {
+public class WebActivity extends AppCompatActivity implements AlertDialogFragment.AlertDialogClickEvent {
 
     private WebView mWebView;
 
@@ -22,8 +24,22 @@ public class WebActivity extends AppCompatActivity {
         Intent intent = getIntent();
         Bundle extra = intent.getExtras();
         String url = (extra != null) ? extra.getString("url") : NYT_HOME_URL;
+        if (url.equals(UNDEFINED)) {
+            AlertDialogFragment alertDialogFragment = AlertDialogFragment.newInstance(AlertDialogFragment.NO_URL);
+            alertDialogFragment.show(getSupportFragmentManager(),"KEY");
+        }
 
         mWebView = findViewById(R.id.web_activity_web_view);
         mWebView.loadUrl(url);
+    }
+
+    @Override
+    public void doPositiveClick() {
+        mWebView.loadUrl(NYT_HOME_URL);
+    }
+
+    @Override
+    public void doNegativeClick() {
+        finish();
     }
 }
